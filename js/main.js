7d1244a6fd08502c3a8363cc1cfd5b09f7102d45
Tatiana Osipova <otv120780@gmail.com>
2023-01-29 19:44:10 -0500

    feat: add icons

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ function showError(errorMessage) {
     header.insertAdjacentHTML('afterend', html);   
 }
 
-function showCard({name, country, temp, condition}) {
+function showCard({name, country, temp, condition, imgPath}) {
     // Markup for the card
     const html = `<div class="card">
                 <h2 class="card-city">${name}
@@ -35,7 +35,7 @@ function showCard({name, country, temp, condition}) {
 
                 <div class="card-weather">
                     <div class="card-value">${temp}<sup>°c</sup></div>
-                    <img class="card-img" src="./img/icon.png" alt="sun and cloud">
+                    <img class="card-img" src="${imgPath}" alt="sun and cloud">
                 </div>
 
                 <div class="card-description">${condition}</div>
@@ -87,14 +87,23 @@ form.onsubmit = async function (e) {
         );
         
         console.log(info);
-        // console.log(info.languages[23]['day_text']);
+        console.log(info.languages[23]['day_text']);
+
+        const filePath = './img/' + (data.current.is_day ? 'day' : 'night') + '/';  
+        const fileName = (data.current.is_day ? info.day : info.night) + '.png';
+        const imgPath = filePath + fileName;
+        console.log('filePath', filePath + fileName);    
+            
 
         const weatherData = {
             name: data.location.name,
             country: data.location.country,
             temp: data.current.temp_c,
-            condition: data.current.condition.text,
-            // condition: info.languages[23]['day_text'],
+            // condition: data.current.condition.text,
+            condition: data.current.is_day 
+                ? info.languages[23]['day_text'] 
+                : info.languages[23]['night_text'],
+            imgPath: imgPath,     
         };
 
         showCard(weatherData);                               
